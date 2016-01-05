23a3299ef28d815c82b495853775451f84f21c27
zubritsky <zubritsky@targetprocess.com>
2016-01-05 16:04:35 +0300

    Adds all resources read methods to Client.fs

---
+++ build/JsonSamplesLoader.fsx
@@ -0,0 +1,24 @@
+﻿#load "RestModel.fsx"
+#load @"..\paket-files\v-zubritsky\fsharp-common\PathUtils.fs"
+#r @"..\packages\Http.fs\lib\net40\HttpClient.dll"
+
+open HttpClient
+open RestModel
+open System.IO
+open FSharpCommon.PathUtils
+
+let curDir = __SOURCE_DIRECTORY__
+let outputPath = curDir @@ @"..\misc\JsonSamples"
+let endpoint = "https://plan.tpondemand.com"
+let resources = getResources()
+
+resources 
+|> List.iter (fun r -> 
+     let resourceUrl = endpoint + r.RelativeUrl
+     let sampleOutputPath = outputPath @@ (sprintf "Get%s.json" r.ResourceName)
+
+     createRequest Get resourceUrl
+     |> withBasicAuthentication "LOGIN" "PASSWORD"
+     |> withHeader (Accept("application/json"))
+     |> getResponseBody
+     |> (fun resourceSample -> File.WriteAllText(sampleOutputPath, resourceSample)))
---
+++ build/RestModel.fsx
@@ -0,0 +1,42 @@
+﻿#r "System.Xml.Linq"
+#I @"..\packages"
+#r @"FSharp.Data\lib\net40\FSharp.Data.dll"
+
+open System.Xml.Linq
+open System.Text.RegularExpressions
+open FSharp.Data
+
+type private ResourcesMeta = XmlProvider<"""..\misc\JsonSamples\meta.xml""">
+
+type ResourceType = Entity | Collection
+
+type ResourceDescriptor = 
+    { EntityName: string;
+      ResourceName: string;
+      RelativeUrl: string;
+      Type: ResourceType;
+      Description: string; }
+
+let private getResourceName(r: ResourcesMeta.ResourceMetadataDescription) = 
+    let uriMatch = r.Uri |> Regex(@".*/(?<Name>.*)/meta").Match
+    uriMatch.Groups.["Name"].Value
+
+let private getResourceRelativeUri(r: ResourcesMeta.ResourceMetadataDescription) = 
+    let uriMatch = r.Uri |> Regex(@"https://plan.tpondemand.com(?<RelativeUri>/api/v1/.*)/meta").Match
+    uriMatch.Groups.["RelativeUri"].Value
+
+let getResources() = 
+    ResourcesMeta.GetSample()
+    |> (fun meta -> meta.ResourceMetadataDescriptions) 
+    |> Array.map (fun r -> 
+         let resourceName = r |> getResourceName
+         // Asssuming that EntityName (ex. Assignable) differs from
+         // ResourceName (ex. Assignables) only because of plural form for collections
+         let resourceType = if resourceName = r.Name then Entity else Collection
+
+         { EntityName = r.Name;
+           ResourceName = resourceName;
+           RelativeUrl = r |> getResourceRelativeUri; 
+           Type = resourceType;
+           Description = r.Description})
+    |> List.ofArray
